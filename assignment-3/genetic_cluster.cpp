f1d099344ae17bfe3eb628b397ee582c02ad00a1
Soumyajit Deb <debsoumyajit100@gmail.com>
2020-04-03 09:54:29 +0530

    Added Assigment 3

--- assignment-3/genetic_cluster.cpp
+++ assignment-3/genetic_cluster.cpp
@@ -15,7 +15,7 @@ typedef struct population population;
 void initial_population(population* populi,int n,int m,int k,int size)
 {
 	srand(time(NULL));
-	
+
 	populi->population_size=size;
 
 	populi->cluster_list.resize(populi->population_size);
@@ -77,6 +77,60 @@ void show_population(population* populi)
 }
 
 
+void fitness_function(population* populi,int n,int m,int k,vector <vector <int> > arr)
+{
+	populi->fitness_value.resize(populi->population_size,0);
+
+	for(int i=0;i<populi->population_size;i++)
+	{
+		for(int j=0;j<k;j++)
+		{
+			vector <int> temp;
+
+			for(int l=0;l<n;l++)
+			{
+				if(populi->cluster_list[i][l]==j)
+				{
+					temp.push_back(l);
+				}
+			}
+
+			int sum=0;
+
+			for(int l=0;l<temp.size();l+=2)
+			{
+				int add=0;
+
+				if(l<temp.size())
+				{
+					for(int t=0;t<m;t++)
+					{
+						add=add + (arr[temp[l+1]][t] -arr[temp[l]][t]);
+					}
+
+					sum=sum+add;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			populi->fitness_value[i]=populi->fitness_value[i]+sum;
+		}
+	}
+}
+
+
+void show_fitness(population* populi)
+{
+	for(int i=0;i<populi->fitness_value.size();i++)
+	{
+		cout<<populi->fitness_value[i]<<" ";
+	}
+
+	cout<<"\n";
+}
 
 int main()
 {
@@ -95,7 +149,14 @@ int main()
 
 	cin>>k;
 
-	int arr[n][m];
+	vector <vector <int> > arr;
+
+	arr.resize(n);
+
+	for(int i=0;i<n;i++)
+	{
+		arr[i].resize(m);
+	}
 
 	for(int i=0;i<n;i++)
 	{
@@ -113,7 +174,9 @@ int main()
 
 	show_population(&populi);
 
+	fitness_function(&populi,n,m,k,arr);
 
+	show_fitness(&populi);
 
 	return 0;
 }
